e7cfa9836bd2f0fec5eab73c3a86e46beb07829b
Alexander Eibner <alexander.eibner@indoqa.com>
2014-03-18 12:55:58 +0100

    Introduced delay for finding process pid after starting office process

--- jodconverter-core/src/test/java/org/artofsolving/jodconverter/office/PooledOfficeManagerTest.java
+++ jodconverter-core/src/test/java/org/artofsolving/jodconverter/office/PooledOfficeManagerTest.java
@@ -12,27 +12,15 @@
 //
 package org.artofsolving.jodconverter.office;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertNotNull;
-import static org.testng.Assert.assertTrue;
-import static org.testng.Assert.fail;
+import static org.testng.Assert.*;
 
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.TimeoutException;
 
-
 import org.artofsolving.jodconverter.ReflectionUtils;
-import org.artofsolving.jodconverter.office.ManagedOfficeProcess;
-import org.artofsolving.jodconverter.office.PooledOfficeManager;
-import org.artofsolving.jodconverter.office.PooledOfficeManagerSettings;
-import org.artofsolving.jodconverter.office.OfficeConnection;
-import org.artofsolving.jodconverter.office.UnoUrl;
-import org.artofsolving.jodconverter.office.OfficeException;
-import org.artofsolving.jodconverter.office.OfficeProcess;
 import org.testng.annotations.Test;
 
-@Test(groups="integration")
+@Test(groups = "integration")
 public class PooledOfficeManagerTest {
 
     private static final UnoUrl CONNECTION_MODE = UnoUrl.socket(2002);
@@ -40,42 +28,46 @@ public class PooledOfficeManagerTest {
 
     public void executeTask() throws Exception {
         PooledOfficeManager officeManager = new PooledOfficeManager(CONNECTION_MODE);
-        ManagedOfficeProcess managedOfficeProcess = (ManagedOfficeProcess) ReflectionUtils.getPrivateField(officeManager, "managedOfficeProcess");
+        ManagedOfficeProcess managedOfficeProcess = (ManagedOfficeProcess) ReflectionUtils.getPrivateField(officeManager,
+                "managedOfficeProcess");
         OfficeProcess process = (OfficeProcess) ReflectionUtils.getPrivateField(managedOfficeProcess, "process");
         OfficeConnection connection = (OfficeConnection) ReflectionUtils.getPrivateField(managedOfficeProcess, "connection");
-        
+
         officeManager.start();
-        assertTrue(process.isRunning());
-        assertTrue(connection.isConnected());
-        
+        assertTrue(process.isRunning(), "Process is not running.");
+        assertTrue(connection.isConnected(), "Connection is not connected");
+
         MockOfficeTask task = new MockOfficeTask();
         officeManager.execute(task);
-        assertTrue(task.isCompleted());
-        
+        assertTrue(task.isCompleted(), "Task is not completed");
+
         officeManager.stop();
-        assertFalse(connection.isConnected());
-        assertFalse(process.isRunning());
+        assertFalse(connection.isConnected(), "Connection is connected");
+        assertFalse(process.isRunning(), "Process is running");
         assertEquals(process.getExitCode(0, 0), 0);
     }
 
     public void restartAfterCrash() throws Exception {
         final PooledOfficeManager officeManager = new PooledOfficeManager(CONNECTION_MODE);
-        ManagedOfficeProcess managedOfficeProcess = (ManagedOfficeProcess) ReflectionUtils.getPrivateField(officeManager, "managedOfficeProcess");
+        ManagedOfficeProcess managedOfficeProcess = (ManagedOfficeProcess) ReflectionUtils.getPrivateField(officeManager,
+                "managedOfficeProcess");
         OfficeProcess process = (OfficeProcess) ReflectionUtils.getPrivateField(managedOfficeProcess, "process");
         OfficeConnection connection = (OfficeConnection) ReflectionUtils.getPrivateField(managedOfficeProcess, "connection");
         assertNotNull(connection);
-        
+
         officeManager.start();
         assertTrue(process.isRunning());
         assertTrue(connection.isConnected());
-        
+
         new Thread() {
+
+            @Override
             public void run() {
                 MockOfficeTask badTask = new MockOfficeTask(10 * 1000);
                 try {
                     officeManager.execute(badTask);
                     fail("task should be cancelled");
-                    //FIXME being in a separate thread the test won't actually fail
+                    // FIXME being in a separate thread the test won't actually fail
                 } catch (OfficeException officeException) {
                     assertTrue(officeException.getCause() instanceof CancellationException);
                 }
@@ -84,19 +76,19 @@ public class PooledOfficeManagerTest {
         Thread.sleep(500);
         Process underlyingProcess = (Process) ReflectionUtils.getPrivateField(process, "process");
         assertNotNull(underlyingProcess);
-        underlyingProcess.destroy();  // simulate crash
+        underlyingProcess.destroy(); // simulate crash
 
         Thread.sleep(RESTART_WAIT_TIME);
-        assertTrue(process.isRunning());
-        assertTrue(connection.isConnected());
+        assertTrue(process.isRunning(), "Process is running");
+        assertTrue(connection.isConnected(), "Connection is connected");
 
         MockOfficeTask goodTask = new MockOfficeTask();
         officeManager.execute(goodTask);
-        assertTrue(goodTask.isCompleted());
+        assertTrue(goodTask.isCompleted(), "Task is completed");
 
         officeManager.stop();
-        assertFalse(connection.isConnected());
-        assertFalse(process.isRunning());
+        assertFalse(connection.isConnected(), "Connection is not connected");
+        assertFalse(process.isRunning(), "Process is not running");
         assertEquals(process.getExitCode(0, 0), 0);
     }
 
@@ -104,16 +96,17 @@ public class PooledOfficeManagerTest {
         PooledOfficeManagerSettings configuration = new PooledOfficeManagerSettings(CONNECTION_MODE);
         configuration.setTaskExecutionTimeout(1500L);
         final PooledOfficeManager officeManager = new PooledOfficeManager(configuration);
-        
-        ManagedOfficeProcess managedOfficeProcess = (ManagedOfficeProcess) ReflectionUtils.getPrivateField(officeManager, "managedOfficeProcess");
+
+        ManagedOfficeProcess managedOfficeProcess = (ManagedOfficeProcess) ReflectionUtils.getPrivateField(officeManager,
+                "managedOfficeProcess");
         OfficeProcess process = (OfficeProcess) ReflectionUtils.getPrivateField(managedOfficeProcess, "process");
         OfficeConnection connection = (OfficeConnection) ReflectionUtils.getPrivateField(managedOfficeProcess, "connection");
         assertNotNull(connection);
-        
+
         officeManager.start();
-        assertTrue(process.isRunning());
-        assertTrue(connection.isConnected());
-        
+        assertTrue(process.isRunning(), "Process is running");
+        assertTrue(connection.isConnected(), "Connection is connected");
+
         MockOfficeTask longTask = new MockOfficeTask(2000);
         try {
             officeManager.execute(longTask);
@@ -123,16 +116,16 @@ public class PooledOfficeManagerTest {
         }
 
         Thread.sleep(RESTART_WAIT_TIME);
-        assertTrue(process.isRunning());
-        assertTrue(connection.isConnected());
+        assertTrue(process.isRunning(), "Process is not running");
+        assertTrue(connection.isConnected(), "Connection is not connected");
 
         MockOfficeTask goodTask = new MockOfficeTask();
         officeManager.execute(goodTask);
-        assertTrue(goodTask.isCompleted());
+        assertTrue(goodTask.isCompleted(), "Task is not completed");
 
         officeManager.stop();
-        assertFalse(connection.isConnected());
-        assertFalse(process.isRunning());
+        assertFalse(connection.isConnected(), "connection still connected");
+        assertFalse(process.isRunning(), "Process still running");
         assertEquals(process.getExitCode(0, 0), 0);
     }
 
@@ -140,16 +133,17 @@ public class PooledOfficeManagerTest {
         PooledOfficeManagerSettings configuration = new PooledOfficeManagerSettings(CONNECTION_MODE);
         configuration.setMaxTasksPerProcess(3);
         final PooledOfficeManager officeManager = new PooledOfficeManager(configuration);
-        
-        ManagedOfficeProcess managedOfficeProcess = (ManagedOfficeProcess) ReflectionUtils.getPrivateField(officeManager, "managedOfficeProcess");
+
+        ManagedOfficeProcess managedOfficeProcess = (ManagedOfficeProcess) ReflectionUtils.getPrivateField(officeManager,
+                "managedOfficeProcess");
         OfficeProcess process = (OfficeProcess) ReflectionUtils.getPrivateField(managedOfficeProcess, "process");
         OfficeConnection connection = (OfficeConnection) ReflectionUtils.getPrivateField(managedOfficeProcess, "connection");
         assertNotNull(connection);
-        
+
         officeManager.start();
         assertTrue(process.isRunning());
         assertTrue(connection.isConnected());
-        
+
         for (int i = 0; i < 3; i++) {
             MockOfficeTask task = new MockOfficeTask();
             officeManager.execute(task);
@@ -162,7 +156,7 @@ public class PooledOfficeManagerTest {
         officeManager.execute(task);
         assertTrue(task.isCompleted());
         int taskCount = (Integer) ReflectionUtils.getPrivateField(officeManager, "taskCount");
-        assertEquals(taskCount, 0);  //FIXME should be 1 to be precise
+        assertEquals(taskCount, 0); // FIXME should be 1 to be precise
 
         officeManager.stop();
         assertFalse(connection.isConnected());
